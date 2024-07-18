63639fe64a71b09e74e2a876cf526e79f7e95f1d
mega-dean <mega_dean@fastmail.com>
2024-07-18 11:31:55 -0500

    log in with username instead of email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,17 +5,28 @@ class SessionsController < Clearance::SessionsController
   end
 
   def create
-    @user = authenticate(params)
+    email = User.find_by(username: params[:session][:username])&.email
 
-    sign_in(@user) do |status|
-      if status.success?
-        redirect_back_or(url_after_create)
-      else
-        flash.now.alert = "flash.session_create_failure"
-        @signing_in = true
+    if email
+      params[:session][:email] = email
 
-        render(template: "sessions/new", status: :unauthorized)
+      @user = authenticate(params)
+
+      sign_in(@user) do |status|
+        if status.success?
+          redirect_back_or(url_after_create)
+        else
+          flash.now.alert = "flash.incorrect_password"
+          @signing_in = true
+
+          render(template: "sessions/new", status: :unauthorized)
+        end
       end
+    else
+      flash.now.alert = I18n.t("username_not_found", username: params[:session][:username])
+      @signing_in = true
+
+      render(template: "sessions/new", status: :unauthorized)
     end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   include Clearance::User
+
+  validates :username, uniqueness: true
 end
